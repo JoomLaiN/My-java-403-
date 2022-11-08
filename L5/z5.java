bfa13c26cf24adb1063ec5742c3b5e59b17cb49b
JoomLaiN <99471301+JoomLaiN@users.noreply.github.com>
2022-11-08 11:13:02 +0700

    Add files via upload

---
+++ L5/z5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[][] array = new int[3][5];
+		System.out.print("Array: \n");
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++) {
+				array[i][j] = (int) ( Math.random() * 10 );
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
